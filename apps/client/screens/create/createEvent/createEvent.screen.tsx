39708a01931af8728c03b792aec5c7b3cb0f4fa5
Milgam06 <milgamfruit@gmail.com>
2026-01-31 01:45:53 +0900

    feat(front): add createEventMutation

--- apps/client/screens/create/createEvent/createEvent.screen.tsx
+++ apps/client/screens/create/createEvent/createEvent.screen.tsx
@@ -1,9 +1,16 @@
-import { Button, Input } from '@/components';
+import { Alert, Button, Input } from '@/components';
 import { memo, useCallback, useState } from 'react';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import RNDateTimePicker, { DateTimePickerEvent } from '@react-native-community/datetimepicker';
-import { Label, Sheet, Stack, Text } from 'tamagui';
+import { Sheet, Stack, Text } from 'tamagui';
 import { Platform } from 'react-native';
+import { Image } from 'expo-image';
+import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome';
+import { faCalendar } from '@fortawesome/free-solid-svg-icons/faCalendar';
+import { faArrowRight } from '@fortawesome/free-solid-svg-icons/faArrowRight';
+import { faClock } from '@fortawesome/free-solid-svg-icons/faClock';
+import { useCreateNewEventMutation } from '@/libs';
+import { useUserStore } from '@/stores';
 
 type IDateTimePickerProps = {
   type: 'date' | 'time';
@@ -14,46 +21,52 @@ type IDateTimePickerProps = {
   onChangeUpdate: (event: DateTimePickerEvent, date?: Date) => void;
 };
 
+type ICreateEventScreenProps = {
+  organizationId: string;
+};
+
 const DateTimePicker = memo<IDateTimePickerProps>(
   ({ type = 'time', value, isShow, onPressShow, onClose, onChangeUpdate }) => {
     const isPlatformAndroid = Platform.OS === 'android';
-
-    const labelContent = type === 'date' ? '날짜 선택' : '시간 선택';
-    const dateTimeContent =
-      type === 'date'
-        ? value.toLocaleDateString('ko-KR')
-        : value.toLocaleTimeString('ko-KR', { hour: '2-digit', minute: '2-digit' });
-
+    const isTypeDate = type === 'date';
+    const labelContent = isTypeDate ? '날짜' : '시간';
+    const dateTimeContext = isTypeDate
+      ? value.toLocaleDateString('ko-KR')
+      : value.toLocaleTimeString('ko-KR', { hour: '2-digit', minute: '2-digit' });
     return (
       <Stack width="$fluid" justify="center" items="flex-start" gap="$size.x1">
-        <Label width="$fit" px="$size.x2" fontSize="$4" fontWeight="$600" lineHeight="$1">
-          {labelContent} *
-        </Label>
-        <Stack width="$fluid" items="center" flexDirection="row" justify="space-between" gap="$size.x5">
+        <Stack
+          width="$fluid"
+          flexDirection="row"
+          justify="space-between"
+          items="center"
+          p="$size.x3"
+          borderWidth={1}
+          borderColor="$colors.lightGray"
+          gap="$size.x3"
+          boxShadow="0 0 10px rgba(0, 0, 0, 0.2)"
+          style={{ borderRadius: 12 }}>
           <Stack
-            flex={1}
-            width="$fluid"
-            px="$size.x3"
-            py="$size.x2"
-            bg="$colors.backgroundWhite"
-            borderWidth={1}
-            borderColor="$colors.mediumGray"
-            style={{ borderRadius: 8 }}>
-            <Text fontSize="$5" fontWeight="700" color="$colors.darkGray">
-              {dateTimeContent}
+            width="$fit"
+            justify="center"
+            items="center"
+            aspectRatio={1}
+            p="$size.x3"
+            bg="$colors.primaryGreen"
+            style={{ borderRadius: 16 }}>
+            <FontAwesomeIcon size={30} icon={isTypeDate ? faCalendar : faClock} color="#EAF8F0" />
+          </Stack>
+          <Stack flex={1} justify="center">
+            <Text fontSize="$4" fontWeight="$500" color="$colors.darkGray">
+              {labelContent}
+            </Text>
+            <Text fontSize="$7" fontWeight="$700" color="$colors.darkGray">
+              {dateTimeContext}
             </Text>
           </Stack>
-          <Button
-            isFullWidth={false}
-            px="$size.x6"
-            py="$size.x2"
-            bg="$colors.primaryGreen"
-            pressStyle={{ bg: '$colors.primaryGreen', scale: 0.99, opacity: 0.8 }}
-            onPress={onPressShow}>
-            <Text fontSize="$5" fontWeight="700" color="$colors.white">
-              선택
-            </Text>
-          </Button>
+          <Stack p="$size.x2" onPress={onPressShow}>
+            <FontAwesomeIcon size={20} icon={faArrowRight} color="#888888" />
+          </Stack>
         </Stack>
         {isPlatformAndroid ? (
           isShow && <RNDateTimePicker value={value} mode={type} display="spinner" onChange={onChangeUpdate} />
@@ -95,11 +108,14 @@ const DateTimePicker = memo<IDateTimePickerProps>(
   },
 );
 
-export const CreateEventScreen = memo(() => {
+export const CreateEventScreen = memo(({ organizationId }: ICreateEventScreenProps) => {
+  const { userId } = useUserStore();
   const [eventTitle, setEventTitle] = useState<string>('');
   const [dateTime, setDateTime] = useState<Date>(new Date());
   const [isShowDatePicker, setIsShowDatePicker] = useState<boolean>(false);
   const [isShowTimePicker, setIsShowTimePicker] = useState<boolean>(false);
+  const [isCreateEventError, setIsCreateEventError] = useState<boolean>(false);
+  const [createNewEventMutation] = useCreateNewEventMutation();
 
   const handleChangeEventTitle = useCallback((text: string) => {
     setEventTitle(text);
@@ -123,7 +139,8 @@ export const CreateEventScreen = memo(() => {
 
   const handleDateChange = useCallback((event: DateTimePickerEvent, selectedDate?: Date) => {
     setIsShowDatePicker(false);
-    if (event.type === 'set' && selectedDate) {
+    const isDateUpdated = event.type === 'set' && selectedDate;
+    if (isDateUpdated) {
       setDateTime((prevDate) => {
         const newDate = new Date(prevDate);
         newDate.setFullYear(selectedDate.getFullYear());
@@ -136,7 +153,8 @@ export const CreateEventScreen = memo(() => {
 
   const handleTimeChange = useCallback((event: DateTimePickerEvent, selectedDate?: Date) => {
     setIsShowTimePicker(false);
-    if (event.type === 'set' && selectedDate) {
+    const isTimeUpdated = event.type === 'set' && selectedDate;
+    if (isTimeUpdated) {
       setDateTime((prevDate) => {
         const newDate = new Date(prevDate);
         newDate.setHours(selectedDate.getHours());
@@ -146,9 +164,58 @@ export const CreateEventScreen = memo(() => {
     }
   }, []);
 
+  const handlePressCloseAlert = useCallback(() => {
+    setIsCreateEventError(false);
+  }, []);
+
+  const handlePressCreateEvent = useCallback(async () => {
+    const { errors: createEventErrors } = await createNewEventMutation({
+      variables: {
+        input: {
+          eventTitle,
+          eventStartAt: dateTime,
+          eventDuration: 0,
+          hostOrganizationId: organizationId,
+          hostUserId: userId,
+        },
+      },
+    });
+    if (createEventErrors) {
+      setIsCreateEventError(true);
+      return;
+    }
+  }, [createNewEventMutation, dateTime, eventTitle, organizationId, userId]);
+
   return (
     <SafeAreaView edges={['top']} style={{ flex: 1 }}>
       <Stack flex={1} width="$fluid" justify="space-between" px="$size.x5" pt="$size.x5" pb="$size.x10" gap="$size.x8">
+        {isCreateEventError && (
+          <Alert isOpen={isCreateEventError} onClose={handlePressCloseAlert} alertPadding="$size.x5">
+            <Stack justify="center" items="center" gap="$size.x6" pt="$size.x3">
+              <Stack justify="center" items="center" gap="$size.x2">
+                <Image
+                  source="https://raw.githubusercontent.com/Tarikul-Islam-Anik/Animated-Fluent-Emojis/master/Emojis/Smilies/Crying%20Face.png"
+                  alt="Crying Face"
+                  style={{ width: 140, aspectRatio: 1 }}
+                />
+                <Text fontSize="$5" fontWeight="$900">
+                  이벤트 생성에 실패했습니다.
+                </Text>
+              </Stack>
+              <Button
+                px="$size.x1"
+                py="$size.x2"
+                bg="$colors.errorRed"
+                fontSize="$7"
+                fontWeight="$600"
+                color="$colors.backgroundWhite"
+                pressStyle={{ bg: '$colors.errorRed', opacity: 0.8 }}
+                onPress={handlePressCloseAlert}>
+                확인했어요
+              </Button>
+            </Stack>
+          </Alert>
+        )}
         <Stack gap="$size.x1">
           <Stack gap="$size.x0_25">
             <Text fontSize="$9" fontWeight="800" color="$colors.black">
@@ -168,26 +235,26 @@ export const CreateEventScreen = memo(() => {
         </Stack>
         <Stack flex={1} justify="space-between">
           <Stack width="$fluid" gap="$size.x5">
-            <Input
-              value={eventTitle}
-              onChangeText={handleChangeEventTitle}
-              w="$fluid"
-              placeholder="이벤트 이름을 입력하세요"
-              labelContent="이벤트 이름 *"
-              size="$x12"
-              px="$size.x3"
+            <Stack
+              width="$fluid"
+              p="$size.x3"
+              borderWidth={1}
               borderColor="$colors.lightGray"
-              fontWeight="500"
-              focusStyle={{ borderColor: '$colors.darkGreen', borderWidth: '$size.x0_5' }}
-            />
-            <Stack width="$fluid" flexDirection="row" items="center" gap="$size.x2">
-              <DateTimePicker
-                value={dateTime}
-                type="time"
-                isShow={isShowTimePicker}
-                onPressShow={handleShowTimePicker}
-                onClose={handleCloseTimePicker}
-                onChangeUpdate={handleTimeChange}
+              gap="$size.x3"
+              boxShadow="0 0 10px rgba(0, 0, 0, 0.2)"
+              style={{ borderRadius: 12 }}>
+              <Input
+                value={eventTitle}
+                onChangeText={handleChangeEventTitle}
+                w="$fluid"
+                placeholder="이벤트 이름을 입력하세요"
+                labelContent="이벤트 이름 *"
+                labelFontColor="$colors.mediumGray"
+                size="$x12"
+                fontSize="$8"
+                px="$size.x2"
+                borderWidth={0}
+                fontWeight="500"
               />
             </Stack>
             <Stack width="$fluid" flexDirection="row" items="center" gap="$size.x2">
@@ -198,6 +265,16 @@ export const CreateEventScreen = memo(() => {
                 onPressShow={handleShowDatePicker}
                 onClose={handleCloseDatePicker}
                 onChangeUpdate={handleDateChange}
+              />
+            </Stack>
+            <Stack width="$fluid" flexDirection="row" items="center" gap="$size.x2">
+              <DateTimePicker
+                value={dateTime}
+                type="time"
+                isShow={isShowTimePicker}
+                onPressShow={handleShowTimePicker}
+                onClose={handleCloseTimePicker}
+                onChangeUpdate={handleTimeChange}
               />
             </Stack>
           </Stack>
